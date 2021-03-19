437dab6451a029e22e2e8db11e8660cef1500ad8
Poonam2482 <66667170+Poonam2482@users.noreply.github.com>
2021-03-19 14:51:15 +0530

    Login_Seleniumtest

---
+++ WebApplication1/LoginSample.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LoginSample.aspx.cs" Inherits="WebApplication1.LoginSample" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="txtun"  runat="server"></asp:TextBox>
+            <asp:TextBox ID="txtpwd" runat="server"></asp:TextBox>
+            <asp:Button ID="btnsubmit" runat="server" Text="Login" OnClick="btnsubmit_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/LoginSample.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class LoginSample : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnsubmit_Click(object sender, EventArgs e)
+        {
+            if (txtun.Text == "")
+            {
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "alertMessage", "alert('Enter UserName')", true);
+            }
+            else
+                if (txtpwd.Text == "")
+            {
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "alertMessage", "alert('Enter password')", true);
+            }
+            else
+                if (txtun.Text == "abc" && txtpwd.Text == "abc")
+            {
+                Response.Redirect("Welcome.aspx");
+            }
+            else
+            {
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "alertMessage", "alert('Incorrect Un or Pwd')", true);
+            }
+        }
+    }
+}
